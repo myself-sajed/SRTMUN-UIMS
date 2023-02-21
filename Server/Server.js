4d3321109b7cefcec466dc15ee21caff5cb84dd5
VishalPanchal <vishalpanchal570@gmail.com>
2023-02-21 09:43:45 +0530

    server.js db comment

--- Server/Server.js
+++ Server/Server.js
@@ -97,8 +97,8 @@ app.use(require('./utility/verifyOTP'));
 
 
 // Database Configuration
-const URL = `mongodb://${process.env.DB_User}:${process.env.DB_Pass}@localhost:27017/${process.env.DB_Name}?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false`;
-// const URL = `mongodb://localhost:27017/srtmun`
+// const URL = `mongodb://${process.env.DB_User}:${process.env.DB_Pass}@localhost:27017/${process.env.DB_Name}?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false`;
+const URL = `mongodb://localhost:27017/srtmun`
 mongoose
   .connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
