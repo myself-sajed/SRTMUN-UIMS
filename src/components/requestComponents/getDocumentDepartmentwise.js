f3a6edf52a3786a6bbaa11d71200b6cd7c1b689d
VishalPanchal <vishalpanchal570@gmail.com>
2023-03-22 23:19:11 +0530

    22-03-2023

--- src/components/requestComponents/getDocumentDepartmentwise.js
+++ src/components/requestComponents/getDocumentDepartmentwise.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const getDepartmentWiseDocumentCount = ({ model, setState, property }) => {
     axios.post(`${process.env.REACT_APP_MAIN_URL}/getDepartmentWiseDocumentCount`,
         { model, property }).then(response => {
-            console.log(response.data)
             setState(response.data)
         }).catch(error => {console.error(error)});
 };
