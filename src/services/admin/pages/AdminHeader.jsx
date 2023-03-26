2f2caae754a99b5e1f3d18d868ce2476ef7540db
VishalPanchal <vishalpanchal570@gmail.com>
2023-03-27 00:20:59 +0530

    AdminHeader 27-03-23

--- src/services/admin/pages/AdminHeader.jsx
+++ src/services/admin/pages/AdminHeader.jsx
@@ -3,10 +3,16 @@ import "../css/admin.css"
 import { Input, Space } from 'antd';
 import { Avatar } from '@mui/material';
 import useAdminAuth from '../../../hooks/useAdminAuth';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { setAdminUser } from '../../../redux/slices/UserSlice';
+import siteLinks from '../../../components/siteLinks';
 const { Search } = Input;
 
 
 const AdminHeader = () => {
+  const navigate = useNavigate()
+    const dispatch = useDispatch()
   useAdminAuth()
   return (
     <div className='main-header'>
@@ -29,10 +35,10 @@ const AdminHeader = () => {
                 <Avatar src={`/public/assets/male.jpg`} className="cursor-pointer" />
               </button>
               <ul className="dropdown-menu">
-              <li><button className="dropdown-item" onClick={() => {}}>Profile</button></li>
+              {/* <li><button className="dropdown-item" onClick={() => {}}>Profile</button></li> */}
                                         
-              <li><hr className="dropdown-divider" /></li>
-              <li><button className="dropdown-item" >Logout</button></li>
+              {/* <li><hr className="dropdown-divider" /></li> */}
+              <li><button className="dropdown-item" style={{color: "red"}} onClick={() => { dispatch(setAdminUser(null)); navigate(siteLinks.welcome.link); localStorage.removeItem('admin-token'); }} >Logout</button></li>
             </ul>
           </div>
         </div>
