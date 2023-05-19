eeaf898f8c100444cf1aa9dfd6615f3359d07829
Shaikh Sajed <shaikhsajedcoolpop@gmail.com>
2023-05-19 15:07:54 +0530

    Show file

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -5,40 +5,10 @@ import DeptSelect from '../inputs/DeptSelect'
 
 const Test = () => {
 
-    const [loading, setLoading] = useState(false)
-
-
     return (
-        <div className='w-full'>
-            <div className='p-2 border rounded-md my-3 bg-gray-50 h-screen'>
-                <div className='w-full bg-gray-200 rounded-md p-2'>
-                    <p className='font-semibold'>Filters</p>
-                    <div className='grid grid-cols-2 gap-4'>
-                        <div className='flex md:flex-row flex-col items-start justify-start gap-2'>
-                            <div className='mt-3 col-md-5'>
-                                <MultipleYearSelect />
-                            </div>
-                            <div>
-                                <DeptSelect allSchoolTag={true} />
-                            </div>
-                        </div>
-                        <div className='flex md:flex-row flex-col items-start justify-end gap-2 mt-4 mr-3'>
-                            <div>
-                                {
-                                    !loading ? <button type="button" class="btn btn-primary bg-primary">Get Data</button> :
-                                        <button class="btn btn-primary" type="button" disabled>
-                                            <span class="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"></span>
-                                            Getting data...
-                                        </button>
-                                }
-                            </div>
-
-
-                            <button type="button" class="btn btn-success bg-success">Download Data in Excel </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div>
+            <p>SHaikh Sajed Ahmed</p>
+            <a href="http://localhost:4000/showFile/1656181558297-Surendra%20PBAS%2021-22%20with%20proofs-20.pdf/faculty"></a>
         </div>
     )
 }
