ae5011e7210a9dfa5180df8e7c30657493a306ce
VishalPanchal <vishalpanchal570@gmail.com>
2023-07-12 18:29:01 +0530

    fedback

--- src/services/feedback/pages/FeedbackDashboard.jsx
+++ src/services/feedback/pages/FeedbackDashboard.jsx
@@ -8,302 +8,88 @@ import { useSelector } from 'react-redux';
 import useDirectorAuth from '../../../hooks/useDirectorAuth'
 import DashboardHeroSection from '../components/DashboardHeroSection';
 import StudentAnalysis from '../analysis/StudentAnalysis';
-import { teacherQuestions } from './TeacherFeedback';
-import { studentQuestions, teacherQuestions as stutechQ } from './StudentFeedback';
-import SchoolsProgram from '../../../components/SchoolsProgram';
-import designationWiseSorting from '../../../js/designationWiseSorting';
-
-
+import AnalyticsRoundedIcon from '@mui/icons-material/AnalyticsRounded';
+import UserLoading from '../../../pages/UserLoading';
+import OtherResponseAnalysis from '../analysis/OtherResponseAnalysis';
+import { SelectStatusYear } from '../../status/pages/StatusPage';
+import { academicYearGenerator } from '../../../inputs/Year';
 
 
 const FeedbackDashboard = () => {
-    title('Feedback Dashboard')
-    useDirectorAuth()
-    const directorUser = useSelector((state) => state.user.directorUser)
+  title('Feedback Dashboard')
+  useDirectorAuth()
+  const directorUser = useSelector((state) => state.user.directorUser)
+  const [year, setYear] = useState(academicYearGenerator(1)[0])
+  const [activeUser, setActiveUser] = useState("Student");
 
-  const generateChartData = (responses) => {
-    const courseRatings = {
-      'Very Good': 0,
-      'Good': 0,
-      'Satisfactory': 0,
-      'Not-Satisfactory': 0
-    };
+  let param = { filter: { schoolName: directorUser?.department, academicYear: year }, feedbackUser: activeUser }
+  const { data, isLoading, refetch } = useQuery([param.model, param], () => getFeedbackData(param))
 
-    const facilityRatings = {
-      'Very Good': 0,
-      'Good': 0,
-      'Satisfactory': 0,
-      'N/A': 0
-    };
 
-    const facilityQuestions = {};
 
-    const teacherData = {};
 
-    responses.forEach((response) => {
-      const teacherNames = response['Tick only those teachers who taught you this year'];
 
-      teacherNames.forEach((teacherName) => {
-        if (!teacherData[teacherName]) {
-          teacherData[teacherName] = {};
+  const [chartData, setChartData] = useState(null)
+
+  useEffect(() => {
+    setChartData(null)
+  }, [activeUser])
+
+  useEffect(() => {
+    refetch()
+  }, [year])
+
+
+  useEffect(() => {
+
+    if (data?.data?.data) {
+      setChartData(() => data?.data?.data?.analysis)
+    }
+  }, [data, activeUser])
+
+
+
+
+  return (
+    <div>
+      <GoBack pageTitle={`Feedback Dashboard for ${directorUser?.department}`} showAvatar={{ photoURL: directorUser.photoURL, userType: 'director' }} />
+
+      <div className="my-2">
+        <SelectStatusYear setYear={setYear} year={year} />
+      </div>
+
+      <div className="mt-4">
+        <DashboardHeroSection year={year} showActive={activeUser} countData={data?.data?.data.dashboardData} isLoading={isLoading} showDetails={true} setActiveUser={setActiveUser} />
+      </div>
+
+      <div>
+        <p className='mb-3 mt-5 text-lg font-bold rounded-md text-center flex items-center justify-center gap-3 p-2 bg-blue-800 text-white'> <AnalyticsRoundedIcon /> {activeUser} Feedback Analysis</p>
+
+        {!chartData ?
+          <div className='my-5'>
+            <UserLoading title="Analyzing data" />
+          </div>
+          :
+          <div>
+            {(activeUser === "Student" && chartData[activeUser]) && <div className="mb-5 w-full">
+              <StudentAnalysis chartData={chartData[activeUser]} />
+            </div>}
+
+            {(activeUser !== "Student" && chartData[activeUser]) && <div className="mb-5">
+              <OtherResponseAnalysis questionsWithData={chartData[activeUser]} />
+            </div>}
+
+          </div>
         }
-
-        const aboutTeacherSection = response[`About the teacher ${teacherName}`];
-
-        Object.entries(aboutTeacherSection).forEach(([question, rating]) => {
-          if (!teacherData[teacherName][question]) {
-            teacherData[teacherName][question] = {
-              'Very Good': 0,
-              'Good': 0,
-              'Satisfactory': 0,
-              'Un-Satisfactory': 0
-            };
-          }
-
-          teacherData[teacherName][question][rating]++;
-        });
-      });
-
-      const courseRating = response['Rate the course']['Overall rating'];
-      courseRatings[courseRating]++;
-
-      const facilitySection = response['Rate the Facilities available'];
-      Object.entries(facilitySection).forEach(([question, rating]) => {
-        if (!facilityQuestions[question]) {
-          facilityQuestions[question] = {
-            'Very Good': 0,
-            'Good': 0,
-            'Satisfactory': 0,
-            'N/A': 0
-          };
-        }
-
-        facilityQuestions[question][rating]++;
-      });
-    });
-
-    const courseData = {
-      labels: Object.keys(courseRatings),
-      datasets: [
-        {
-          data: Object.values(courseRatings),
-          backgroundColor: ['green', 'blue', 'yellow', 'red']
-        }
-      ]
-    };
-
-    const facilityData = {
-      labels: Object.keys(facilityRatings),
-      datasets: [
-        {
-          data: Object.values(facilityRatings),
-          backgroundColor: ['green', 'blue', 'yellow', 'red']
-        }
-      ]
-    };
-
-    const facilityChartData = Object.entries(facilityQuestions).map(([question, ratings]) => {
-      return {
-        question,
-        data: {
-          labels: Object.keys(ratings),
-          datasets: [
-            {
-              data: Object.values(ratings),
-              backgroundColor: ['green', 'blue', 'yellow', 'red']
-            }
-          ]
-        }
-      };
-    });
-
-    return { courseData, facilityData, facilityChartData, teacherData };
-  };
+      </div>
 
 
-
-
-
-
-
-
-
-
-
-
-
-    let param = { filter: { schoolName: directorUser?.department } }
-
-    const { data, isLoading, refetch } = useQuery([param.model, param], () => getFeedbackData(param))
-    const [teachers, setTeachers] = useState([])
-
-    const [ activeUser, setActiveUser] = useState("Student");
-
-    useEffect(() => {
-      setTeachers(designationWiseSorting(data?.data?.data?.Faculties)?.map((teacher) => `${teacher.salutation} ${teacher.name}`))
-  }, [data])
-  
-    const generalQuestions = [
-      {
-          type: 'check',
-          required: true,
-          question: 'Tick only those teachers who taught you this year',
-          options: teachers,
-      },
-      {
-          type: 'radio',
-          required: true,
-          question: 'Choose the program you are currently enrolled in',
-          options: SchoolsProgram?.[directorUser?.department]?.map((program) => program[0])
-      },
-      {
-          type: 'table',
-          required: true,
-          question: 'Rate the course',
-          head: ['Very Good', 'Good', 'Satisfactory', 'Not-Satisfactory'],
-          cell: ['Depth of the course content including project work if any', 'Extent of coverage of course', 'Applicability/relevance to real life situations', 'Learning value (in terms of knowledge, concepts, manual skills, analytical abilities and broadening perspectives', 'Clarity and relevance of textual reading', 'Relevance of additional source material (Library)', 'Extent of effort required by students', 'Overall rating']
-      },
-      {
-          type: 'table',
-          required: true,
-          question: 'Rate the Facilities available',
-          head: ['Very Good', 'Good', 'Satisfactory', 'N/A'],
-          cell: ['Sufficient number of prescribed books are available in the Library.', 'The books prescribed/listed as reference materials are relevant, updated and appropriate', 'Infrastructural facilities, such as student’s room/ girls room/carrels, class rooms, reading rooms and toilets are available in the Department.', 'Laboratory facilities / Field Visits provided	']
-      },
-      {
-          type: 'check',
-          required: true,
-          question: 'Is your background benefiting from this course?',
-          options: ['More than adequate', 'Just adequate', 'Adequate', 'Inadequate']
-      },
-      {
-          type: 'check',
-          required: true,
-          question: 'The syllabus was?',
-          options: ['Challenging', 'Good', 'Adequate', 'Inadequate']
-      },
-      {
-          type: 'check',
-          required: true,
-          question: 'How helpful was the internal assesssment?',
-          options: ['Highly', 'Moderately', 'Fairly', 'Not sure']
-      },
-      {
-          type: 'radio',
-          required: true,
-          question: 'Were are you provided with a course and lecture outline at the beginning?',
-          options: ['Yes', 'No']
-      },
-      {
-          type: 'radio',
-          required: true,
-          question: 'If Yes, was it followed?',
-          options: ['Yes', 'No']
-      },
-      {
-          type: 'radio',
-          required: true,
-          question: 'If followed, was it helpful?',
-          options: ['Yes', 'No']
-      },
-      {
-          type: 'text',
-          required: true,
-          question: 'If you have other major comments to offer',
-      }
-
-  ]
-
-    useEffect(() => {
-        // setTeacherData(data?.data?.data?.Teacher);
-        generateAnalyticsData(data?.data?.data[activeUser])
-        
-    },[data])
-    
-    function generateAnalyticsData(userData) {
-        
-        const Analiticsdata = activeUser=="Teacher"?{ tNameArr:[], tDigiArr:[0, 0, 0, 0, 0], tContArr:[], tRadioArr : Object.fromEntries(teacherQuestions[3].cell.map((question) => [question, [0, 0, 0, 0, 0]])), tCommentArr:[] }:activeUser=="Student"?{ sNameArr:[], sEmailArr:[], sContArr:[], sProgramEnroled: Array((generalQuestions[1].options)?.length).fill(0) }:null
-
-        let sTeacherTick =  Array((generalQuestions[0].options)?.length).fill(0)
-
-        userData?.forEach((user) => {
-          const parseData = JSON.parse(user.response);
-          
-            const Teacher  = {
-                question0 : parseData[teacherQuestions[0].question],
-                question1 : parseData[teacherQuestions[1].question],
-                question2 : parseData[teacherQuestions[2].question],
-                question3 : parseData[teacherQuestions[3].question],
-                question4 : parseData[teacherQuestions[4].question],
-            }
-            const Student = {
-              question0 : parseData[studentQuestions[0].question],
-              question1 : parseData[studentQuestions[1].question],
-              question2 : parseData[studentQuestions[2].question],
-              question3: parseData[generalQuestions[0].question],
-              question4: parseData[generalQuestions[1].question],
-            }
-           function pushInArray(arr, value) {
-              arr.push(value)
-           }
-           function sQIncriment( arr, que, opt ) {
-            opt.forEach((e, i) => {
-              if (e === que) {
-                arr[i] += 1;
-              }
-            });
-           }
-           function mQIncriment( arr, que, opt ) {
-            opt.head.forEach((e, i) => {
-              opt.cell.forEach((cellItem) => {
-                if (que[cellItem] === e) {
-                  arr[cellItem][i] += 1;
-                }
-              });
-            });
-           }
-          if(activeUser=="Teacher"){
-            pushInArray(Analiticsdata.tNameArr, Teacher.question0);
-            sQIncriment(Analiticsdata.tDigiArr, Teacher.question1, teacherQuestions[1]?.options);
-            pushInArray(Analiticsdata.tContArr,Teacher.question2);
-            mQIncriment(Analiticsdata.tRadioArr, Teacher.question3, teacherQuestions[3]);
-            pushInArray(Analiticsdata.tCommentArr, Teacher.question4);
-          }
-          else if(activeUser=="Student"){
-            // console.log(parseData[stutechQ[0].question]);
-            pushInArray(Analiticsdata.sNameArr, Student.question0);
-            pushInArray(Analiticsdata.sEmailArr, Student.question1);
-            pushInArray(Analiticsdata.sContArr, Student.question2);
-            for(const faculty of Student.question3){
-              const index = (generalQuestions[0].options)?.indexOf(faculty);
-              if(index > -1) {
-                sTeacherTick[index] += 1;
-              }
-            }
-            sQIncriment(Analiticsdata.sProgramEnroled, Student.question4, generalQuestions[1].options)
-            
-          }
-        });
-        // console.log(Analiticsdata.sProgramEnroled);
-        // console.log(sTeacherTick)
-        // console.log(Analiticsdata)
-
-      }
-
-    return (
-        <div>
-            <GoBack pageTitle="Feedback Response" />
-
-            <div className="mt-4">
-                <DashboardHeroSection countData={data?.data?.data} isLoading={isLoading} />
-            </div>
-            {/* <div className="my-5">
-                <StudentAnalysis studentData={data?.data?.data?.Student} isLoading={isLoading} />
-            </div> */}
-        </div>
-    )
+    </div >
+  )
 }
 
 export default FeedbackDashboard
+
+
 
 
