2c1a5899912121cf60e03951865169f34af8b79f
Shaikh Sajed <shaikhsajedcoolpop@gmail.com>
2023-08-30 13:47:29 +0530

    Done Registration

--- src/services/programs/pages/RegistrationAck.jsx
+++ src/services/programs/pages/RegistrationAck.jsx
@@ -22,14 +22,16 @@ const RegistrationAck = () => {
                     program ? <div>
                         <div>
                             <p>Thank you for your successful registration for the program titled
-                                "<p className="text-lg">
-                                    <b className="text-green-800">{program?.title}".</b>
+                                <p className="text-lg">
+                                    "<b className="text-green-800">{program?.title}</b>"
                                 </p>
                             </p>
                             <p className='my-3'>Date of the Program:
                                 <p className="text-lg">
-                                    <b className="text-green-800"> {program?.duration} </b>
+                                    <b className="text-green-800"> {program?.programDate} </b>
                                 </p>
+                                <p className="mt-4">Organized by -- <b> {program?.arrangedBy} </b> </p>
+                                <p> on the venue -- <b> {program?.venue} </b> </p>
                             </p>
                         </div>
                     </div> : <p className="my-5 text-center">Sorry, this page is not available. The session has expired.</p>
