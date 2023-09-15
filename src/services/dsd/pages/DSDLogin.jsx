184c09f3c3b991374551eef24651fce168c793ed
Shaikh Sajed <shaikhsajedcoolpop@gmail.com>
2023-09-15 16:24:51 +0530

    Added youth festival route in DSD login

--- src/services/dsd/pages/DSDLogin.jsx
+++ src/services/dsd/pages/DSDLogin.jsx
@@ -13,6 +13,8 @@ import { setDSDUser } from '../../../redux/slices/UserSlice';
 import { useDispatch } from 'react-redux';
 import useOtherServiceAuth from '../../../hooks/useOtherServiceAuth';
 import { dsdAuthParams } from './DSDHome';
+import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
+import { IconButton } from '@mui/material';
 
 
 
@@ -25,26 +27,57 @@ const DSDLogin = () => {
     const dispatch = useDispatch()
     title(siteLinks.dsdLogin.title)
     useOtherServiceAuth(dsdAuthParams)
-
-
+    const [step, setStep] = useState(1)
+    const [userType, setUserType] = useState(null)
 
     const handleSubmit = (e) => {
         e.preventDefault();
         serviceLoginHandler({ email: username, password, model: "DSDUser", setUser: setDSDUser, navigate, navigationLink: siteLinks.dsdHome.link, tokenName: "dsd-token", setIsLoading, dispatch })
     }
 
+    console.log(step)
+
 
     return (
         <div>
+
             <CredSkeleton bred={[siteLinks.welcome, siteLinks.dsdLogin]} onSubmit={handleSubmit} linkLine={null} head={<CredHeading spacing="mb-3 mt-6" icon={<AutoGraphRoundedIcon className="text-orange-700" />}
                 title={siteLinks.dsdLogin.title} />}>
 
-                <CredInput state={username} setState={setUsername} placeholder="Enter your email" type="text" spacing="mb-2" size={39} />
 
-                <CredInput state={password} setState={setPassword} placeholder="Enter Password" type="password" spacing="mb-3" />
+                {
+                    step === 2 && <div className='float-left'><IconButton onClick={() => { setStep(1) }}><ArrowBackRoundedIcon /></IconButton></div>
+                }
 
-                <CredButton title="Login" isLoading={isLoading} />
+                {
+                    step === 1 && <div>
+                        <p className="text-center my-3">Choose Teacher Type</p>
+                        <div className="flex lg:flex-row flex-col items-stretch justify-center gap-2">
+                            <button className={`bg-blue-600 text-white rounded-lg hover:bg-blue-700 ease-in-out 
+            duration-200 p-3`} onClick={() => { setUserType('DSD'); setStep(2) }}>Director of DSD Login</button>
+                            <button className={`bg-blue-600 text-white rounded-lg hover:bg-blue-700 ease-in-out 
+            duration-200 p-3`} onClick={() => { setUserType('YF'); setStep(2) }}>College Youth Festival</button>
 
+                        </div>
+
+
+                    </div>
+                }
+
+                {
+                    (step === 2 && userType === 'DSD') ? <>
+                        <CredInput state={username} setState={setUsername} placeholder="Enter your email" type="text" spacing="mb-2" size={39} />
+
+                        <CredInput state={password} setState={setPassword} placeholder="Enter Password" type="password" spacing="mb-3" />
+
+                        <CredButton title="Login" isLoading={isLoading} />
+                    </> : (userType === 'YF' && step === 2) && <div className='flex flex-col justify-center items-center gap-2'>
+                        <button className={`bg-blue-600 text-white rounded-lg hover:bg-blue-700 ease-in-out 
+            duration-200 p-3`} onClick={() => { navigate(siteLinks.yfCollegeLogin.link) }}>Youth Festival College Login</button>
+                        <button className={`bg-blue-600 text-white rounded-lg hover:bg-blue-700 ease-in-out 
+            duration-200 p-3`} onClick={() => { navigate(siteLinks.yfCollegeRegistration.link) }}>Youth Festival College Registration</button>
+                    </div>
+                }
             </CredSkeleton>
         </div>
     )
