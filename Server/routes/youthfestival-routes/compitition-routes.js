58c8340819508dafb2b4d4f3f209f335d22bac92
VishalPanchal <vishalpanchal570@gmail.com>
2023-09-16 14:48:29 +0530

    added route

--- Server/routes/youthfestival-routes/compitition-routes.js
+++ Server/routes/youthfestival-routes/compitition-routes.js
@@ -1,8 +1,13 @@
 const fs = require('fs');
 const express = require('express');
 const router = express.Router();
+const YfCompitition = requre('../../models/youth-festival/yfCompetitionSchema.js')
+const YfStudents = require('../../models/youth-festival/yfStudentSchema')
 
-
-router.post('/youthfestival/competition/add', (req,res)=>{
-    console.log(req.body);
+router.post('/youthfestival/competition/:action', async(req,res)=>{
+    const action = req.params.action
+    const { college, selectedStudents, competitionName, academicYear, isGroup } = req.body
+    
 })
+
+module.exports = router
