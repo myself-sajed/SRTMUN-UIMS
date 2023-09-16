fbc9c08b91def0646640bb1c0bed1d9b2b389edf
Shaikh Sajed <shaikhsajedcoolpop@gmail.com>
2023-09-16 14:58:26 +0530

    Done

--- src/services/youthfestival/js/competitionHandler.js
+++ src/services/youthfestival/js/competitionHandler.js
@@ -1,4 +1,4 @@
-
+import Axios from 'axios'
 const addCompetition = async ({ college, selectedStudents, competitionName, academicYear, isGroup, clearFunction, isLoading }) => {
     const link = `${process.env.REACT_APP_MAIN_URL}/youthfestival/competition/add`
     Axios.post(link, { college, selectedStudents, competitionName, academicYear, isGroup })
