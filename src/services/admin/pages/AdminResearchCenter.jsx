010eb69f6bb75b7b5b1b86cb7dce38b7c672db7a
VishalPanchal <vishalpanchal570@gmail.com>
2023-09-24 14:46:39 +0530

    reserch center corrected

--- src/services/admin/pages/AdminResearchCenter.jsx
+++ src/services/admin/pages/AdminResearchCenter.jsx
@@ -4,6 +4,7 @@ import ScienceIcon from '@mui/icons-material/Science';
 import MenuBookIcon from '@mui/icons-material/MenuBook';
 import PlagiarismIcon from '@mui/icons-material/Plagiarism';
 import DocumentScannerIcon from '@mui/icons-material/DocumentScanner';
+import PersonSearchIcon from '@mui/icons-material/PersonSearch';
 import AdminSchoolSelect from '../components/AdminSchoolSelect';
 import GoBack from '../../../components/GoBack';
 import axios from 'axios';
@@ -11,6 +12,8 @@ import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import AcadmicYearSelect from '../components/AcadmicYearSelect';
 import { CircularProgress } from '@mui/material';
+import FileViewer from '../../../components/FileViewer';
+import sortByAcademicYear from '../../../js/sortByAcademicYear';
 
 const AdminResearchCenter = ({ isDirector = false }) => {
 
@@ -18,13 +21,16 @@ const AdminResearchCenter = ({ isDirector = false }) => {
 
   const [values, setValues] = useState({ schoolName: School ? School : "All Schools", yearFilter: [] })
   const { schoolName, yearFilter } = values
-  const [active, setActive] = useState("BooksAndChapters")
+  const [active, setActive] = useState("JrfSrf")
+  const [tableCells, setTableCells] = useState([])
+
 
   const Btns ={
-    "BooksAndChapters":{tableHead:{},name:"Books And Chapters", icon: <MenuBookIcon sx={{fontSize: "30px !important"}}/>},
-    "ResearchProjects": {tableHead:{},name:"Research Projects", icon: <ScienceIcon sx={{fontSize: "30px !important"}} />},
-    "ResearchPapers": {tableHead:{},name:"Research Papers", icon: <PlagiarismIcon sx={{fontSize: "30px !important"}}/>},
-    "Patent": {tableHead:{},name:"Patents", icon: <DocumentScannerIcon sx={{fontSize: "30px !important"}}/>}
+    "JrfSrf":{name:"JRF, SRF, PDF, RA", icon: <PersonSearchIcon sx={{fontSize: "30px !important"}}/>, tableHead:{ index: 'Sr.No.', facultyName: 'Faculty Name', School: "School", researchName: "Reserchar Name", enrolmentYear: "Enrollment Date", fellowshipDuration: "Fellowship Duration", fellowshipType: "Fellowship Type", grantingAgency: "Granting Agency", qualifyingExam: "Qualifying Exam", year: "Academic Year", proof: "Uploaded Proof", },},
+    "BooksAndChapters":{name:"Books And Chapters", icon: <MenuBookIcon sx={{fontSize: "30px !important"}}/>, tableHead:{ index: 'Sr.No.', facultyName: 'Faculty Name', School: "School", type: 'Type', titleOfBook: 'Title of Book / Chapter / Edited Book / Translation', chapterTitle: "Title of Chapter / Translation", paperTitle: 'Paper Title', transType: "Translation work", titleOfProceeding: 'Title of proceedings of the conference', conName: 'Conference Name', isNat: 'Wheather National / International', publicationYear: 'Year of Publication', issnNumber: 'ISBN/ISSN number of proceeding', aff: 'Affiliation Institute at the time of publication',publisherName: 'Publisher Name', year: 'Academic Year',  proof: "Uploaded Proof" },},
+    "ResearchProjects": {name:"Research Projects", icon: <ScienceIcon sx={{fontSize: "30px !important"}} />, tableHead:{index: 'Sr.No.', facultyName: 'Faculty Name', School: "School", schemeName: 'Scheme / Project Title', principalName: 'Principal Invigilator', coInvestigator: 'Co-Invigilator', fundingName: 'Funding Agency', isGov: 'Govt. / Non-Govt.', awardYear: 'Award Year', providedFunds: 'Funds (INR)', fundType: 'Major / Minor', status: 'Project Status', duration: 'Project Duration', year: 'Academic Year', proof: 'Upload Proof',},},
+    "ResearchPapers": {name:"Research Papers", icon: <PlagiarismIcon sx={{fontSize: "30px !important"}}/>, tableHead:{ index: "Sr.No.", facultyName: 'Faculty Name', School: "School", paperTitle:'Paper Title', journalName:'Journal Name', authors:'Author(s)', publicationYear:'Publication Year', issnNumber:'ISSN Number', indexedIn:'Indexed in', indexLink:'Links', year: 'Academic Year', proof: 'Upload Proof'},},
+    "Patent": {name:"Patents", icon: <DocumentScannerIcon sx={{fontSize: "30px !important"}}/>, tableHead: { index: 'Sr.No.', facultyName: 'Faculty Name', School: "School", patentNumber: 'Patent Number', patentTitle: 'Patent Title', isNat: 'Wheather National / International', awardYear: 'Award Year of Patent', year: 'Academic Year', proof: 'Upload Proof' }}
   }
 
   const getReserchData = async (filter) => {
@@ -36,17 +42,23 @@ const AdminResearchCenter = ({ isDirector = false }) => {
 
   const { data, isLoading, refetch} = useQuery(['getReserchData', schoolName, yearFilter], () => getReserchData({schoolFilter, academicYearFilter}))
 
-  useEffect(()=>{
-    refetch()
-  },[schoolName, yearFilter])
+  // useEffect(()=>{
+  //   refetch()
+  // },[schoolName, yearFilter])
 
-  console.log(data);
+  useEffect(()=>{
+    let Cells = Object.keys(Btns[active].tableHead)
+    const itemsToRemove = ["index","facultyName","School","proof"]
+    setTableCells(Cells.filter(item => !itemsToRemove.includes(item))) 
+  },[active])
+
+  console.log(data?.data?.[active])
   return (
     <div>
       {isDirector && <div className='mb-4'><GoBack pageTitle={`${School} Research Center`} /></div>}
       <AdminDrower hideHeader={School ? true : false}>
         <div className='sub-main'>
-        <div className='flex justify-end pb-2'>
+        <div className='flex px-3 flex-wrap gap-2'>
             <AcadmicYearSelect className="col-md-4 col-lg-4 col-12" value={yearFilter} setState={setValues} id="yearFilter" label="Filter By Academic Year" />
             {!School && <>
               <AdminSchoolSelect className="col-md-4 col-lg-4 col-12" value={schoolName} setState={setValues} id="schoolName" label="Filter By School" />
@@ -55,15 +67,15 @@ const AdminResearchCenter = ({ isDirector = false }) => {
           <div className='flex flex-wrap w-full'>
             {Object.keys(Btns).map((e,i)=>{
               return(
-                <div className='col-12 col-sm-6 col-lg-3 px-1 h-[90px]' onClick={()=>{setActive(e)}} key={i}>
-                <div className={`w-full h-full flex flex-row ${active===e?'bg-[#c19038]':'bg-[#f1f3f4]'} rounded-md border-t-4 border-[#deb66f] ${active===e?'text-[#f1f3f4]':'text-[#ae7e28]'} cursor-pointer`}>
-                  <div className='bg-red h-full w-1/4 flex justify-end items-center p-3'>{Btns[e].icon}</div>
-                  <div className='bg-red h-full w-3/4 flex flex-col'>
-                    <div className='h-1/2 w-full flex items-end text-[18px] font-semibold' >{Btns[e].name}</div>
-                    <div className='h-1/2 w-full flex items-start font-semibold text-[14px]'>{data?.data?data.data[e].length:<CircularProgress color="inherit" size={18} />}</div>
+                <div className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 px-1 pb-1 h-[90px]' onClick={() => { setActive(e) }} key={i}>
+                  <div className={`w-full h-full flex flex-row ${active === e ? 'bg-[#c19038]' : 'bg-[#f1f3f4]'} rounded-md border-t-4 border-[#deb66f] ${active === e ? 'text-[#f1f3f4]' : 'text-[#ae7e28]'} cursor-pointer`}>
+                    <div className='bg-red h-full w-1/4 flex justify-end items-center p-3'>{Btns[e].icon}</div>
+                      <div className='bg-red h-full w-3/4 flex flex-col'>
+                      <div className='h-1/2 w-full flex items-end text-[15px] font-semibold'>{Btns[e].name}</div>
+                      <div className='h-1/2 w-full flex items-start font-semibold text-[14px]'>{isLoading ? <CircularProgress color="inherit" size={18} /> : data.data[e].length}</div>
+                    </div>
                   </div>
                 </div>
-              </div>
               )
             })
               
@@ -73,31 +85,33 @@ const AdminResearchCenter = ({ isDirector = false }) => {
                 <table className="table">
                   <thead className="sticky-top" style={{ background: "#ae7e28", color: '#FFF' }}>
                     <tr>
-                      <th>Sr. No.</th>
-                      <th>profile Pic</th>
-                      <th>Name</th>
-                      <th>School</th>
-                      <th>Gender</th>
-                      <th>Email</th>
-                      <th>Eanroled Program</th>
-                      <th>Program Complited on</th>
-
+                      {
+                        Object.keys(Btns[active].tableHead).map((e, i) => <th key={i}>{Btns[active].tableHead[e]}</th>)
+                      }
                     </tr>
                   </thead>
                   <tbody>
-                    {/* {
-                      tileData?.map((item, index) => <tr>
+                  {data?.data && sortByAcademicYear(data?.data?.[active], 'year')?.map((item, index) => {
+                    let facultyName
+                    let school
+                    if (item.userId !== undefined && item.userId !== null) {
+                      facultyName = `${item.userId.salutation} ${item.userId.name}`;
+                      school = item.userId.department;
+                    }
+                    else if(item.userId === undefined){
+                      facultyName = item.guideName
+                      school = item.schoolName;
+                    }
+                    return (
+                      <tr key={index}>
                         <td>{index + 1}</td>
-                        <td><Avatar src={`${process.env.REACT_APP_MAIN_URL}/showFile/${item.photoURL}/student`} /></td>
-                        <td>{`${item.salutation} ${item.name}`}</td> 
-                        <td>{item.schoolName}</td>
-                        <td>{item.gender}</td>
-                        <td>{item.email}</td>
-                        <td>{item.programGraduated}</td>
-                        <td>{model==="StudentUser"?item.programEnroledOn:item.programCompletedOn}</td>
+                        <td>{facultyName}</td>
+                        <td>{school}</td>
+                        {tableCells?.map((e, i) => <td key={i}>{item[e]}</td>)}
+                        <td><FileViewer fileName={item.proof} serviceName="faculty" /></td>
                       </tr>
-                      )
-                    } */}
+                    );
+                  })}
                   </tbody>
                 </table>
               </div>
