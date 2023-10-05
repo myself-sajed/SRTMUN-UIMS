df457cf9d1a4223f0111ff6d58c0df6db853ea10
myself-sajed <shaikhsajedcoolpop@gmail.com>
2023-10-05 14:13:24 +0530

    added 2023-24 academic year in numerical dashboard

--- src/services/admin/pages/AdminNumaricalData.jsx
+++ src/services/admin/pages/AdminNumaricalData.jsx
@@ -28,7 +28,7 @@ const AdminNumaricalData = ({ isDirector = false }) => {
   const [module, setModule] = useState(null)
   const [model, setModel] = useState(null)
   const [proof, setProof] = useState(null)
-  const fiveYears = academicYearGenerator(5, true)
+  const fiveYears = academicYearGenerator(5, true, true)
   const generateAcademicYears = [...fiveYears, 'Total']
 
   const tableHead = {
