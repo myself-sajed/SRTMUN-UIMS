fa8c560a8bafea68944a47f2e369dadbe98f4dd4
VishalPanchal <vishalpanchal570@gmail.com>
2023-11-07 18:43:23 +0530

    bulk table data entry done

--- src/components/BulkTableEntry.jsx
+++ src/components/BulkTableEntry.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import { useEffect } from 'react';
-import toast from 'react-hot-toast';
+// import toast from 'react-hot-toast';
 import { TextareaAutosize } from '@mui/material';
 
 function BulkTableEntry({ tableHead, typeObject, tableData, setTableData, model }) {
   const [filds, setFilds] = useState("");
-  const [columnToFill, setColumnToFill] = useState("");
+  // const [columnToFill, setColumnToFill] = useState("");
 
   useEffect(() => {
     setTableData((pri) => {
@@ -39,74 +39,74 @@ function BulkTableEntry({ tableHead, typeObject, tableData, setTableData, model 
   //   console.log('Table data is:', tableData)
   // }, [tableData])
 
-  const HandleDataPaste = async (e) => {
-    e.preventDefault();
-    const pastedData = e.clipboardData.getData('text').trim();
-    let filteredData = [];
+  // const HandleDataPaste = async (e) => {
+  //   e.preventDefault();
+  //   const pastedData = e.clipboardData.getData('text').trim();
+  //   let filteredData = [];
 
-    if (pastedData) {
-      const cleanedData = pastedData.replace(/\r/g, '');
+  //   if (pastedData) {
+  //     const cleanedData = pastedData.replace(/\r/g, '');
 
-      if (cleanedData.includes("\n")) {
-        filteredData = cleanedData.split('\n');
-      } else if (cleanedData.includes("\t")) {
-        filteredData = cleanedData.split('\t');
-      }
+  //     if (cleanedData.includes("\n")) {
+  //       filteredData = cleanedData.split('\n');
+  //     } else if (cleanedData.includes("\t")) {
+  //       filteredData = cleanedData.split('\t');
+  //     }
 
-      if (filteredData.includes("") || columnToFill === "") {
-        toast.error(filteredData.includes("") ? "Randomly cell selection or Empty cell selection not allowed" : "Column Field can not be empty");
-        filteredData = [];
-      }
-      console.log(filteredData);
+  //     if (filteredData.includes("") || columnToFill === "") {
+  //       toast.error(filteredData.includes("") ? "Randomly cell selection or Empty cell selection not allowed" : "Column Field can not be empty");
+  //       filteredData = [];
+  //     }
+  //     console.log(filteredData);
 
-      if (filteredData.length > 0) {
-        for (let i = 0; i < filteredData.length; i++) {
-          if (tableData.length === i) {
-            // Asynchronously set the new state and initialize the missing elements
-            await setTableData((prevData) => [
-              ...prevData,
-              ...Array.from({ length: filteredData.length - prevData.length }, () => ({})),
-            ]);
-          }
+  //     if (filteredData.length > 0) {
+  //       for (let i = 0; i < filteredData.length; i++) {
+  //         if (tableData.length === i) {
+  //           // Asynchronously set the new state and initialize the missing elements
+  //           await setTableData((prevData) => [
+  //             ...prevData,
+  //             ...Array.from({ length: filteredData.length - prevData.length }, () => ({})),
+  //           ]);
+  //         }
 
-          if (typeObject[columnToFill] === "date") {
-            const parsedDate = new Date(filteredData[i]);
-            if (!isNaN(parsedDate.getTime())) {
-              // Update the state with the formatted date
-              setTableData((prevData) => [
-                ...prevData.slice(0, i), // Copy data before the current index
-                { ...prevData[i], [columnToFill]: parsedDate.toISOString().slice(0, 10) }, // Update the current index
-                ...prevData.slice(i + 1), // Copy data after the current index
-              ]);
-            }
-          }
-          else if (Array.isArray(typeObject[columnToFill])) {
-            let arr = typeObject[columnToFill];
-            setTableData((prevData) => [
-              ...prevData.slice(0, i),
-              { ...prevData[i], [columnToFill]: arr.includes(filteredData[i]) ? filteredData[i] : "" },
-              ...prevData.slice(i + 1),
-            ]);
-          }
-          else {
-            // Update the state with the non-date value
-            setTableData((prevData) => [
-              ...prevData.slice(0, i), // Copy data before the current index
-              { ...prevData[i], [columnToFill]: filteredData[i] }, // Update the current index
-              ...prevData.slice(i + 1), // Copy data after the current index
-            ]);
-          }
-        }
-      } else {
-        toast.error("no data");
-      }
-    }
-  };
+  //         if (typeObject[columnToFill] === "date") {
+  //           const parsedDate = new Date(filteredData[i]);
+  //           if (!isNaN(parsedDate.getTime())) {
+  //             // Update the state with the formatted date
+  //             setTableData((prevData) => [
+  //               ...prevData.slice(0, i), // Copy data before the current index
+  //               { ...prevData[i], [columnToFill]: parsedDate.toISOString().slice(0, 10) }, // Update the current index
+  //               ...prevData.slice(i + 1), // Copy data after the current index
+  //             ]);
+  //           }
+  //         }
+  //         else if (Array.isArray(typeObject[columnToFill])) {
+  //           let arr = typeObject[columnToFill];
+  //           setTableData((prevData) => [
+  //             ...prevData.slice(0, i),
+  //             { ...prevData[i], [columnToFill]: arr.includes(filteredData[i]) ? filteredData[i] : "" },
+  //             ...prevData.slice(i + 1),
+  //           ]);
+  //         }
+  //         else {
+  //           // Update the state with the non-date value
+  //           setTableData((prevData) => [
+  //             ...prevData.slice(0, i), // Copy data before the current index
+  //             { ...prevData[i], [columnToFill]: filteredData[i] }, // Update the current index
+  //             ...prevData.slice(i + 1), // Copy data after the current index
+  //           ]);
+  //         }
+  //       }
+  //     } else {
+  //       toast.error("no data");
+  //     }
+  //   }
+  // };
 
   return (
     <div>
 
-      <div className='flex w-full'>
+      {/* <div className='flex w-full'>
         <div className='col-12 col-md-6 col-lg-4 px-1'>
           <label htmlFor="colSelect" className="form-label">Select Column</label>
           <select
@@ -125,7 +125,7 @@ function BulkTableEntry({ tableHead, typeObject, tableData, setTableData, model 
           <label htmlFor="dataSetter" className="form-label">Paste Here</label>
           <input className='form-control' id='dataSetter' onPaste={HandleDataPaste} />
         </div>
-      </div>
+      </div> */}
       <div className='table-responsive mt-4' style={{ maxHeight: "80vh" }}>
         <table className="table table-bordered" >
           <thead className="sticky-top bg-blue-600 text-[#fff]" >
