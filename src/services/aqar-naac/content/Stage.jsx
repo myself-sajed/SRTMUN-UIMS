5873c39a47fedb3c849952d6519f5e901bfd7455
VishalPanchal <vishalpanchal570@gmail.com>
2023-11-20 11:33:49 +0530

    added atable in specifyed creteria

--- src/services/aqar-naac/content/Stage.jsx
+++ src/services/aqar-naac/content/Stage.jsx
@@ -40,18 +40,13 @@ const Stage = () => {
     const users = useSelector((state) => state.user)
 
 
+    console.log(users);
+
+
     const AQARTables = {
-        director: [
+        criterion1: [
             {
-                title: 'Syllabus Revision',
-                hasSupportingDocument: true,
-                proofData: {
-                    academicYear, proofType: 'SyllabusRevision', userType: 'director'
-                },
-                component: <SyllabusRevision filterByAcademicYear={true} academicYear={academicYear} />
-            },
-            {
-                title: 'Employability',
+                title: 'Employability 1.2.1',
                 hasSupportingDocument: true,
                 proofData: {
                     academicYear, proofType: 'Employability', userType: 'director'
@@ -59,69 +54,153 @@ const Stage = () => {
                 component: <Employability filterByAcademicYear={true} academicYear={academicYear} />
             },
             {
-                title: 'Value Added Courses',
+                title: 'Syllabus Revision 1.2.2',
+                hasSupportingDocument: true,
+                proofData: {
+                    academicYear, proofType: 'SyllabusRevision', userType: 'director'
+                },
+                component: <SyllabusRevision filterByAcademicYear={true} academicYear={academicYear} />
+            },
+            {
+                title: 'Value Added Courses 1.3.3',
+                hasSupportingDocument: true,
+                proofData: {
+                    academicYear, proofType: 'ValueAddedCource', userType: 'director'
+                },
                 component: <ValueAddedCource filterByAcademicYear={true} academicYear={academicYear} />
             },
             {
-                title: 'Projects / Internships',
+                title: 'Projects / Internships 1.3.4',
+                hasSupportingDocument: true,
+                proofData: {
+                    academicYear, proofType: 'ProjectsInternships', userType: 'director'
+                },
                 component: <ProjectsInternships filterByAcademicYear={true} academicYear={academicYear} />
             },
+        ],
+        criterion2: [
             {
-                title: 'Demand Ratio',
+                title: 'Demand Ratio 2.1.1',
+                hasSupportingDocument: true,
+                proofData: {
+                    academicYear, proofType: 'DemandRatio', userType: 'director'
+                },
                 component: <DemandRatio filterByAcademicYear={true} academicYear={academicYear} />
             },
             {
-                title: 'Seats reserved for various categories as per applicable reservation policy',
+                title: 'Seats reserved for various categories as per applicable reservation policy 2.1.2',
+                hasSupportingDocument: true,
+                proofData: {
+                    academicYear, proofType: 'ReservedSeats', userType: 'director'
+                },
                 component: <ReservedSeats filterByAcademicYear={true} academicYear={academicYear} />
             },
+        ],
+        criterion3: [
             {
-                title: 'UGC-SAP, CAS, DST-FIST, DBT, ICSSR',
+                title: 'UGC-SAP, CAS, DST-FIST, DBT, ICSSR 3.1.6',
+                hasSupportingDocument: true,
+                proofData: {
+                    academicYear, proofType: 'UgcSapCasDstFistDBTICSSR', userType: 'director'
+                },
                 component: <UgcSapCasDstFistDbtICssr filterByAcademicYear={true} academicYear={academicYear} />
             },
             {
-                title: 'Research Methodology Workshops',
+                title: 'Research Methodology Workshops 3.3.2',
+                hasSupportingDocument: true,
+                proofData: {
+                    academicYear, proofType: 'ResearchMethodologyWorkshops', userType: 'director'
+                },
                 component: <ResearchMethodologyWorkshops filterByAcademicYear={true} academicYear={academicYear} />
             },
             {
-                title: 'Awards',
+                title: 'Awards 3.3.3',
+                hasSupportingDocument: true,
+                proofData: {
+                    academicYear, proofType: 'Award', userType: 'director'
+                },
                 component: <Awards filterByAcademicYear={true} academicYear={academicYear} />
             },
             {
-                title: 'Extension Activities',
+                title: 'Extension Activities 3.6.3',
+                hasSupportingDocument: true,
+                proofData: {
+                    academicYear, proofType: 'ExtensionActivities', userType: 'director'
+                },
                 component: <ExtensionActivities filterByAcademicYear={true} academicYear={academicYear} />
             },
             {
-                title: 'Memorandum of Understanding (MoU)',
+                title: 'Memorandum of Understanding (MoU) 3.7.2',
+                hasSupportingDocument: true,
+                proofData: {
+                    academicYear, proofType: 'MoUs', userType: 'director'
+                },
                 component: <MoUs filterByAcademicYear={true} academicYear={academicYear} />
             },
+        ],
+        criterion5: [
             {
-                title: 'Counseling and Guidance',
+                title: 'Counseling and Guidance 5.1.2',
+                hasSupportingDocument: true,
+                proofData: {
+                    academicYear, proofType: 'CounselingAndGuidance', userType: 'director'
+                },
                 component: <CounselingAndGuidance filterByAcademicYear={true} academicYear={academicYear} />
             },
             {
-                title: 'Skills Enhancement Initiatives',
+                title: 'Skills Enhancement Initiatives 5.1.3',
+                hasSupportingDocument: true,
+                proofData: {
+                    academicYear, proofType: 'SkillsEnhancementInitiatives', userType: 'director'
+                },
                 component: <SkillsEnhancementInitiatives filterByAcademicYear={true} academicYear={academicYear} />
             },
             {
-                title: 'Qualified Exams',
+                title: 'Qualified Exams 5.2.1',
+                hasSupportingDocument: true,
+                proofData: {
+                    academicYear, proofType: 'QualifiedExams', userType: 'director'
+                },
                 component: <QualifiedExams filterByAcademicYear={true} academicYear={academicYear} />
             },
             {
-                title: 'Placements',
+                title: 'Placements 5.2.2',
+                hasSupportingDocument: true,
+                proofData: {
+                    academicYear, proofType: 'Placement', userType: 'director'
+                },
                 component: <Placements filterByAcademicYear={true} academicYear={academicYear} />
             },
             {
-                title: 'Progression to Higher Education',
+                title: 'Progression to Higher Education 5.2.3',
+                hasSupportingDocument: true,
+                proofData: {
+                    academicYear, proofType: 'ProgressionToHE', userType: 'director'
+                },
                 component: <ProgressionToHE filterByAcademicYear={true} academicYear={academicYear} />
             },
+        ],
+        criterion6: [
             {
-                title: 'Alumni Contribution',
-                component: <AlumniContribution filterByAcademicYear={true} academicYear={academicYear} />
-            },
-            {
-                title: 'Professional Development / Administrative Training programs organized',
+                title: 'Professional Development / Administrative Training programs organized 6.3.3',
+                hasSupportingDocument: true,
+                proofData: {
+                    academicYear, proofType: 'TrainingProgramsOrganized', userType: 'director'
+                },
                 component: <TrainingProgramsOrganized filterByAcademicYear={true} academicYear={academicYear} />
             },
+        ],
+        director: [
+            
+            {
+                title: 'Alumni Contribution',
+                hasSupportingDocument: true,
+                proofData: {
+                    academicYear, proofType: 'ProjectsInternships', userType: 'director'
+                },
+                component: <AlumniContribution filterByAcademicYear={true} academicYear={academicYear} />
+            },
+            
 
         ]
     }
