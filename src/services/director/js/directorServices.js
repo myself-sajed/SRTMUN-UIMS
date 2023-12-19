79fd8c2f2acaf44e0e3e106d81b229c9ba5dd2bb
myself-sajed <shaikhsajedcoolpop@gmail.com>
2023-12-19 13:54:08 +0530

    student intake done

---
+++ src/services/director/js/directorServices.js
@@ -0,0 +1,47 @@
+import siteLinks from "../../../components/siteLinks";
+
+const directorServices = [
+    {
+        title: "School Student Management",
+        abbv: "SSM",
+        link1: {
+            title: "Go to SSM",
+            link: siteLinks.ssm.link
+        }
+    },
+    {
+        title: "National Institutional Ranking Framework",
+        abbv: "NIRF",
+        link1: {
+            title: "Go to NIRF Report",
+            link: siteLinks.nirfSelectYear.link
+        }
+    },
+    {
+        title: "Annual Quality Assurance Report",
+        abbv: "AQAR",
+        link1: {
+            title: "Fill Form",
+            link: siteLinks.directorAqar.link
+        },
+    },
+    {
+        title: "Faculty Data Center",
+        abbv: "FDC",
+        link1: {
+            title: "Get Faculty Data",
+            link: siteLinks.fdc.link
+        },
+    },
+    {
+        title: "Faculty Data Center",
+        abbv: "FDC",
+        link1: {
+            title: "Get Faculty Data",
+            link: siteLinks.fdc.link
+        },
+    },
+];
+
+
+export default directorServices
